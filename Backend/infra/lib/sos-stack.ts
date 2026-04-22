fb06a158a5f9f23224230be65f8a8a32638c8de4
Ennio Haibach <ennio.haibach@porsche.de>
2026-04-22 11:14:22 +0200

    fix: [SW-110] Remove unused var

--- Backend/infra/lib/sos-stack.ts
+++ Backend/infra/lib/sos-stack.ts
@@ -26,7 +26,7 @@ export class SosStack extends cdk.Stack {
     const { appUsersTable, pushNotificationTopic, deviceTokensTable } = props;
 
     const sosEventsTable = new dynamodb.Table(this, 'app-sos-events-table', {
-      tableName: `${prefix}AppSOSEvents`,
+      tableName: `AppSOSEvents`,
       partitionKey: {
         name: 'sosId',
         type: dynamodb.AttributeType.STRING,
@@ -78,14 +78,14 @@ export class SosStack extends cdk.Stack {
     });
 
     const sosPropagationQueue = new sqs.Queue(this, 'sos-propagation-queue', {
-      queueName: `${prefix}safewalk-sos-propagation-queue`,
+      queueName: `safewalk-sos-propagation-queue`,
       visibilityTimeout: cdk.Duration.seconds(60),
       retentionPeriod: cdk.Duration.hours(1),
       receiveMessageWaitTime: cdk.Duration.seconds(20),
     });
 
     this.sosHandler = new NodejsFunction(this, 'app-sos-handler', {
-      functionName: `${prefix}app-sos-handler`,
+      functionName: `app-sos-handler`,
       runtime: lambda.Runtime.NODEJS_24_X,
       handler: 'index.handler',
       entry: path.join(__dirname, '../../lambda/sos-handler/index.ts'),
